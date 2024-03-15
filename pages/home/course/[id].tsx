9ed40607662b0253d437f647aece790175aa7101
YactayoC <syactayocrisostomo@gmail.com>
2024-03-14 23:11:44 -0500

    Add react-simple-star-rating package

--- pages/home/course/[id].tsx
+++ pages/home/course/[id].tsx
@@ -5,19 +5,21 @@ import Loader from 'components/loader/loader';
 import styles from 'styles/Home.module.css';
 import { CursoItemI } from 'interfaces/cursoItem';
 import { useEffect, useState } from 'react';
-import { getCommentsByIdCourse, addComment, deleteComment } from 'services/comment';
+import { getCommentsByIdCourse, addComment } from 'services/comment';
 import { Comentario } from 'interfaces/comment';
 import { useForm } from 'react-hook-form';
 import { useAtom } from 'jotai';
 import { userAtom } from 'store/userAtom';
+import { Rating } from 'react-simple-star-rating'
+
 interface Props {
   courseContent: CursoItemI;
   isLoading: boolean;
 }
 
-
 const CoursesSelectedPage: NextPage<Props> = ({ courseContent, isLoading = true }) => {
   const [comentarios, setComentarios] = useState<Comentario[]>([]);
+  const [rating, setRating] = useState(0)
   const [user] = useAtom(userAtom);
   const [courseId, setCourseId] = useState('');
   const [isLoggedIn, setIsLoggedIn] = useState(false); // Estado inicial falso
@@ -29,13 +31,20 @@ const CoursesSelectedPage: NextPage<Props> = ({ courseContent, isLoading = true 
     reset,
   } = useForm<Comentario>();
 
-  //console.log(courseContent)
+  const handleRating = (rate: number) => {
+    setRating(rate);
+  }
 
   const handleGetCommentsByIdCourse = async (idCourse: string) => {
     const response = await getCommentsByIdCourse(idCourse);
     setComentarios(response.comentarios);
     setCourseId(String(courseContent.id));
   }
+
+  const onPointerEnter = () => console.log('Enter')
+  const onPointerLeave = () => console.log('Leave')
+  const onPointerMove = (value: number, index: number) => console.log(value, index)
+
   if (isLoading) {
     return <Loader />;
   }
@@ -84,108 +93,123 @@ const CoursesSelectedPage: NextPage<Props> = ({ courseContent, isLoading = true 
           <Navbar />
         </div>
       </HomeLayout>
+      {/* VIDEO Y DESCRIPCION */}
       <div
         style={{
           display: 'flex',
-          flexDirection: 'column',
+          flexDirection: 'row-reverse',
           alignItems: 'center',
-          rowGap: '2rem',
+          columnGap: '10rem',
           marginTop: '3rem',
           marginBottom: '5rem',
-          padding: '0 20rem'
+          padding: '0 10rem'
         }}
       >
-        <iframe width="1400" height="600" src={convertLinkEmbed(courseContent.video_iframe)} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-        <h1
-          style={{
-            fontSize: '3rem',
-            fontWeight: 'bold',
-          }}
-        >{courseContent.nombre}</h1>
-        <p
-          style={{
-            textAlign: 'center',
-          }}
-        >{courseContent.descripcion}</p>
+        <iframe width="6500" height="600" src={convertLinkEmbed(courseContent.video_iframe)} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+
         <div
           style={{
-            width: '80%',
             display: 'flex',
             flexDirection: 'column',
-            alignItems: 'center',
-            rowGap: '2rem',
+            alignItems: 'flex-start',
           }}
         >
-
-          {isLoggedIn ? (
-            <form style={{ width: '80%', display: 'flex', flexDirection: 'column' }} onSubmit={handleSubmit(handleAddComment)}>
-              <div className="input-group mb-3">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Inserte comentario"
-                  aria-label="Recipient's username"
-                  aria-describedby="button-addon2"
-                  style={{
-                    height: '3.5rem',
-                  }}
-                  {...register('comentario', { required: true })} // Vincula el input al estado con useForm
-                />
-                <button className="btn btn-outline-secondary" type="submit" id="button-addon2">Guardar comentario</button>
-              </div>
-              {errors.comentario && <span style={{ color: 'red', marginTop: '-1rem' }}>Este campo es requerido.</span>}
-            </form>) : (<></>)}
-          <div
+          <h1
             style={{
-              width: '100%',
-              // backgroundColor: '#F5F5F5',
-              padding: '3rem',
-              borderRadius: '0.8rem',
+              fontSize: '3rem',
+              fontWeight: 'bold',
             }}
-          >
-            {comentarios.map((comment) => (
-              <div key={comment.id} className="media mb-4" style={{
-                padding: '1.8rem 2rem',
-                boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-                borderRadius: '0.8rem',
-                backgroundColor: '#F5F5F5',
-              }}>
-                <img
-                  src="https://via.placeholder.com/64"
-                  className="mr-3 rounded-circle"
-                  alt="Avatar"
-                  style={{ width: '64px', height: '64px' }}
-                />
-                <div className="media-body">
-                  <div
+          >{courseContent.nombre}</h1>
+          <Rating
+            style={{ marginBottom: '1rem' }}
+            onClick={handleRating}
+            onPointerEnter={onPointerEnter}
+            onPointerLeave={onPointerLeave}
+            onPointerMove={onPointerMove}
+          />
+          {/* RATING BOOTSTRAP */}
+          <p>{courseContent.descripcion}</p>
+        </div>
+
+      </div>
+      {/* COMENTARIO */}
+      <div
+        style={{
+          padding: '0 15rem',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+
+        {isLoggedIn ? (
+          <form style={{ width: '80%', display: 'flex', flexDirection: 'column' }} onSubmit={handleSubmit(handleAddComment)}>
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Inserte comentario"
+                aria-label="Recipient's username"
+                aria-describedby="button-addon2"
+                style={{
+                  height: '3.5rem',
+                }}
+                {...register('comentario', { required: true })} // Vincula el input al estado con useForm
+              />
+              <button className="btn btn-outline-secondary" type="submit" id="button-addon2">Guardar comentario</button>
+            </div>
+            {errors.comentario && <span style={{ color: 'red', marginTop: '-1rem' }}>Este campo es requerido.</span>}
+          </form>) : (<></>)}
+        <div
+          style={{
+            width: '100%',
+            // backgroundColor: '#F5F5F5',
+            padding: '3rem',
+            borderRadius: '0.8rem',
+          }}
+        >
+          {comentarios.map((comment) => (
+            <div key={comment.id} className="media mb-4" style={{
+              padding: '1.8rem 2rem',
+              boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+              borderRadius: '0.8rem',
+              backgroundColor: '#F5F5F5',
+            }}>
+              <img
+                src="https://via.placeholder.com/64"
+                className="mr-3 rounded-circle"
+                alt="Avatar"
+                style={{ width: '64px', height: '64px' }}
+              />
+              <div className="media-body">
+                <div
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    columnGap: '1rem',
+                  }}
+                >
+                  <h5
                     style={{
-                      display: 'flex',
-                      flexDirection: 'row',
-                      columnGap: '1rem',
+                      marginBottom: '0',
                     }}
-                  >
-                    <h5
-                      style={{
-                        marginBottom: '0',
-                      }}
-                    >{comment.nombre}</h5>
-                    <p
-                      style={{
-                        color: 'gray',
-                      }}
-                    >{convertToHour(comment.fecha)}</p>
-                  </div>
+                  >{comment.nombre}</h5>
                   <p
                     style={{
-                      marginBottom: '0.5rem',
-                      marginTop: '0.5rem',
+                      color: 'gray',
                     }}
-                  >{comment.comentario}</p>
-                  <small className="text-muted">Subido el día {convertToDate(comment.fecha)}</small>
+                  >{convertToHour(comment.fecha)}</p>
                 </div>
+                <p
+                  style={{
+                    marginBottom: '0.5rem',
+                    marginTop: '0.5rem',
+                  }}
+                >{comment.comentario}</p>
+                <small className="text-muted">Subido el día {convertToDate(comment.fecha)}</small>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
       </div>
     </>
